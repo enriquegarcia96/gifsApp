6c59c0bb1457fc13263967c2dd50c608707e0a6c
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-01-04 17:23:00 -0600

    refactor: 💡 metodo de buscar y limpiar el input

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,19 +1,26 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../service/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
-  templateUrl: './busqueda.component.html'
+  templateUrl: './busqueda.component.html',
 })
 export class BusquedaComponent {
-
   /**
    * Agarro el valo de la caja de texto
    */
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  buscar(){
+  constructor(private gifsService: GifsService) {}
+
+  buscar() {
     const valor = this.txtBuscar.nativeElement.value;
+
+    if (valor.trim().length === 0) {
+      return;
+    }
+
+    this.gifsService.buscarGifs(valor);
     this.txtBuscar.nativeElement.value = '';
   }
-
 }
